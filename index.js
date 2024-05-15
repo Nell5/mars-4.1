6d95c3aefd2c7fea6da1593865b3cf0668f091a5
Nell5 <neeennever@gmail.com>
2024-05-15 13:50:32 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+console.log("proekt ishga tushdi");
+
+// confirm("Bu saytdan foydalanish pulli.Davom etasizmi?");
+
+// alert("Bu yerda alert ishlatildi");
+
+// console.log("proekst ishga tushdi");
+
+let ism = prompt("ismigizni kiriting")
+
+console.log("Saytimizga Xush Kelibsiz", ism)
+
+let fam = prompt("familiyangizni kiriting")
+
+console.log("Saytimizga xush kelibsiz", fam)
+
+let age = prompt("yoshingizni kiriting")
+
+console.log("Saytimizga xush kelibsiz", age)
+
+let loc = prompt("Yashash joyingizni kiriting")
+
+console.log("Saytimizga xush kelibsiz", loc)
+
+let fan = prompt("yaxshi korgan fanningiz nima?")
+
+console.log("Saytimizga xush kelibsiz", fan)
